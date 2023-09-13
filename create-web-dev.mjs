8555f1040c7bebfd1f7b755cae86dd1507c80079
Kalepu Satya Sai Teja <mail@kssaiteja.me>
2023-09-13 16:32:34 +0530

    interaction added

--- create-web-dev.mjs
+++ create-web-dev.mjs
@@ -15,36 +15,81 @@ async function generateFiles(projectName) {
 
     // Create index.html
     const htmlContent = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>${projectName || 'Web Dev Start'}</title>
-</head>
-<body>
-    <script src="index.js"></script>
-</body>
-</html>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Hello World Interactive</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1 id="message">Hello, World!</h1>
+            <button id="changeMessage">Change Message</button>
+        </div>
+        <script src="index.js"></script>
+    </body>
+    </html>
+    
 `;
 
     fs.writeFileSync(path.join(projectDirectory, 'index.html'), htmlContent);
 
     // Create style.css
     const cssContent = `
-/* Add your CSS styles here */
-body {
-    background-color: #f0f0f0;
-}
+    body {
+        font-family: Arial, sans-serif;
+        background-color: #f2f2f2;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+    }
+    
+    .container {
+        text-align: center;
+        background-color: #fff;
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    }
+    
+    h1 {
+        color: #333;
+        font-size: 24px;
+        margin: 10px 0;
+    }
+    
+    button {
+        background-color: #007BFF;
+        color: #fff;
+        border: none;
+        padding: 10px 20px;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 16px;
+    }
+    
+    button:hover {
+        background-color: #0056b3;
+    }
+    
 `;
 
     fs.writeFileSync(path.join(projectDirectory, 'style.css'), cssContent);
 
     // Create index.js
     const jsContent = `
-// Add your JavaScript code here
-console.log('Hello from index.js');
+    const messageElement = document.getElementById("message");
+    const changeMessageButton = document.getElementById("changeMessage");
+    changeMessageButton.addEventListener("click", () => {
+    const newMessage = prompt("Enter a new message:");
+    if (newMessage) {
+        messageElement.textContent = newMessage;
+    }
+});
 `;
 
     fs.writeFileSync(path.join(projectDirectory, 'index.js'), jsContent);
